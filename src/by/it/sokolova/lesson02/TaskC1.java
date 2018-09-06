d4ef97322452b2633e2615c47cf551a0a9ca64d2
DashikS <6038137@gmail.com>
2018-09-06 21:27:46 +0300

    C1 +

--- src/by/it/sokolova/lesson02/TaskC1.java
+++ src/by/it/sokolova/lesson02/TaskC1.java
@@ -1,5 +1,9 @@
 package by.it.sokolova.lesson02;
 
+import com.sun.source.doctree.SummaryTree;
+
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -7,12 +11,19 @@ package by.it.sokolova.lesson02;
 34 26
 
 Вывод (это должна появится в консоли, обратите внимание на пробелы и слово Sum:
-Sum = 26
+Sum = 60
 */
 class TaskC1 {
 
+    public static void main(String[] args) {
+        System.out.println("Введите два числа: ");
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        int n=sc.nextInt();
+        int Sum;
+        Sum = i + n;
+        System.out.println("Sum" + " " + "=" + " " + Sum);
 
-
-
+    }
 
 }
