26b1ada8de0e62c4e8c9b1af254b194a9d91aa3e
DashikS <6038137@gmail.com>
2018-09-06 22:22:34 +0300

    C2 +

--- src/by/it/sokolova/lesson02/TaskC2.java
+++ src/by/it/sokolova/lesson02/TaskC2.java
@@ -13,7 +13,19 @@ BIN:100010+11010=111100
 HEX:22+1a=3c
 OCT:42+32=74
 */
+import java.util.Scanner;
+
 class TaskC2 {
-
-
+    public static void main(String[] args) {
+        System.out.println("Введите два числа: ");
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int n = sc.nextInt();
+        int Sum;
+        Sum = i + n;
+        System.out.println("DEC:" + i + "+" + n + "=" + Sum);
+        System.out.println("BIN:" + Integer.toBinaryString(i) + "+" + Integer.toBinaryString(n) + "=" + Integer.toBinaryString(Sum));
+        System.out.println("HEX:" + Integer.toHexString(i) + "+" + Integer.toHexString(n) + "=" + Integer.toHexString(Sum));
+        System.out.println("OCT:" + Integer.toOctalString(i) + "+" + Integer.toOctalString(n) + "=" + Integer.toOctalString(Sum));
+    }
 }
