ebb1c27dea41e7425cefeb3abd4f51f3cafec110
khviasko <n.khviasko@gmail.com>
2018-09-06 21:00:20 +0300

    level A is completed

---
+++ src/by/it/khviasko/lesson02/TaskA2.java
@@ -0,0 +1,22 @@
+package by.it.khviasko.lesson02;
+
+/*
+напишите тело main программы которая выводит на экран 5 строк
+"Я начинаю изучать Java!"
+
+Для ручной проверки запустите программу Ctrl+Shift+F10
+Для автоматической проверки откройте и запустите класс Testing
+*/
+
+class TaskA2 {
+    private static String s ="Я начинаю изучать Java!";
+     public static void main(String[] args)
+    {
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+        System.out.println(s);
+    }
+
+}
