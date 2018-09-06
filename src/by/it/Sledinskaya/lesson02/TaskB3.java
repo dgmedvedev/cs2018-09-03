9d04e857d33fed27d79f843b698bd2b74467a751
sledinskaya <irynasledinskaya@gmail.com>
2018-09-06 21:10:35 +0300

    Tasks B are ready

--- src/by/it/Sledinskaya/lesson02/TaskB3.java
+++ src/by/it/Sledinskaya/lesson02/TaskB3.java
@@ -10,15 +10,9 @@ package by.it.Sledinskaya.lesson02;
 */        
 class TaskB3 {
         public static void main(String[] args) {
-//        String s = "C";
-//        System.out.println("Новым Годом");
-//        System.out.println("C Новым");
-//        System.out.println("Годом");
-//        System.out.print("C Новым");
-//        System.out.println("Годом");
-//        System.out.println(s);
-//        System.out.print("C Новым!");
-//        System.out.print(" ");
-//        System.out.println("Годом");
+
+       System.out.print("C Новым");
+       System.out.print(" ");
+       System.out.println("Годом");
     }
 }
