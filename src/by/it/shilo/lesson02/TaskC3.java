5b06e54da52c226b5e00df72293c7fef86b78145
ivan <shiloivan1997@gmail.com>
2018-09-07 12:22:21 +0300

    lessonABC

--- src/by/it/shilo/lesson02/TaskC3.java
+++ src/by/it/shilo/lesson02/TaskC3.java
@@ -41,6 +41,23 @@ package by.it.shilo.lesson02;
 
         }
         }*/
+//
+import java.util.Scanner;
 class TaskC3 {
+  static double getWeight(int weight){
+      double result=weight*3.86/9.81;
+      double r100=result*100;
+      double delta=r100-(int) r100;
+      if(delta<0.5)
+          return((int)r100)/100.0;
+      else
+          return((int)r100+1)/100.0;
+  }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        System.out.println(getWeight(m));
+    }
 
 }
