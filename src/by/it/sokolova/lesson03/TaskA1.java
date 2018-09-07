368a611108374f35dbcb94563ed5eccc9232c1a4
DashikS <6038137@gmail.com>
2018-09-07 12:07:29 +0300

    lesson03 A1 ok

--- src/by/it/sokolova/lesson03/TaskA1.java
+++ src/by/it/sokolova/lesson03/TaskA1.java
@@ -33,6 +33,24 @@ Scanner sc=new Scanner(System.in);
 
  */
 
-class TaskA1 {
 
+import java.util.Scanner;
+
+class TaskA1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
+        int mod=a%b;
+        System.out.println((a+b)+" "+sub+" "+mul+" "+div+" "+mod);
+        double dsum=(double)a+b;
+        double dsub=(1.0*a)-b;
+        double dmul=(double)a*b;
+        double ddiv=(double)a/b;
+        double dmod=(double)a%b;
+        System.out.println(dsum+" "+dsub+" "+dmul+" "+ddiv+" "+dmod);
+    }
 }
