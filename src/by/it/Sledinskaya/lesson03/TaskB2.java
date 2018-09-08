ab103f62c4fbfdee2a105ecc9f7d6f31105cc37e
sledinskaya <irynasledinskaya@gmail.com>
2018-09-08 21:46:31 +0300

    B1

--- src/by/it/Sledinskaya/lesson03/TaskB2.java
+++ src/by/it/Sledinskaya/lesson03/TaskB2.java
@@ -29,6 +29,29 @@ Lesson 03. Task B2. Нужно написать программу, котора
 Отрицательный дискриминант
 
 */
+import java.util.Scanner;
+
 class TaskB2 {
 
+   public static void main (String []args) {
+       Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       int b=sc.nextInt();
+       int c=sc.nextInt();
+       double d = b * b - 4 * a * c;
+       if (d>0) {
+           double x1, x2;
+           x1 = (-b + Math.sqrt(d)) / (2 * a);
+           x2 = (-b - Math.sqrt(d)) / (2 * a);
+           System.out.println(x1 + " " + x2);
+       }
+       else if (d == 0) {
+           double x;
+           x = -b / (2 * a);
+           System.out.println(x);
+       }
+       else {
+           System.out.println("Отрицательный дискриминант");
+       }
+   }
 }
