ebf227fc7a2147919b1653693f2b7f873234253d
Takosha <42931039+Takosha@users.noreply.github.com>
2018-09-10 00:02:13 +0300

    L2      a1 a2 a3 b1 b2 c1?(спросить нет ли опечатки) L3      a1 a2 b1? b2?

--- src/by/it/takoy/lesson03/TaskB2.java
+++ src/by/it/takoy/lesson03/TaskB2.java
@@ -36,7 +36,7 @@ class TaskB2 {
         Scanner sc = new Scanner(System.in);
         double a = sc.nextDouble();
         double b = sc.nextDouble();
-        double c = sc.nextDouble();
+        double d = sc.nextDouble();
         System.out.println();
     }
 
