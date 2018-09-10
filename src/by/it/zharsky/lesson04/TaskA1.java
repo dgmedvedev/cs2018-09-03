3ea2de2b28a516d555bd37fbc8ab58832edc2270
zharsky <vadimzharsky1990@gmail.com>
2018-09-10 12:40:04 +0300

    lesson4undone

--- src/by/it/zharsky/lesson04/TaskA1.java
+++ src/by/it/zharsky/lesson04/TaskA1.java
@@ -25,6 +25,20 @@ package by.it.zharsky.lesson04;
 
  */
 public class TaskA1 {
+    public static void main(String[] args)
+    {
+        for(int i = 1; i<=10; i++)
+        {
+            if (i == 3||i== 9)
+            {
+                System.out.println("Меня интересует высокая зарплата!");
+            }
+            else
+            {
+                System.out.println("Я изучаю программирование.");
+            }
+        }
+    }
 
 
 }
