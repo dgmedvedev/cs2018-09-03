e56ab389507fdf26b13ceaea344e72e68a6d74da
KsyushaDrebenchuk <drebenchuk.ksysha@gmail.com>
2018-09-12 00:45:37 +0300

    lesson 04 taskA complete TaskB1?

--- src/by/it/drebenchuk/lesson04/TaskB1.java
+++ src/by/it/drebenchuk/lesson04/TaskB1.java
@@ -31,13 +31,9 @@ public class TaskB1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int y = sc.nextInt();
-        if (y % 4 != 0)
-        { System.out.println("количество дней в году: 365");}
-        else {
-            if (y % 100 == 0 && y % 400 == 0)
-            {System.out.println("количество дней в году: 366");}
-            else
-                {System.out.println("количество дней в году: 365");}
-            }
+        if ((y%4==0) && (y % 100 == 0) && (y % 400 == 0) )
+        { System.out.println("количество дней в году: 366");}
+        else
+            {System.out.println("количество дней в году: 365");}
         }
     }
