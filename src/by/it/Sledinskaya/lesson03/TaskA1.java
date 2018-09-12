ab36b36e15120ee2559ac73b620647834fe0e532
sledinskaya <irynasledinskaya@gmail.com>
2018-09-12 11:53:56 +0300

    Lesson03 All

--- src/by/it/Sledinskaya/lesson03/TaskA1.java
+++ src/by/it/Sledinskaya/lesson03/TaskA1.java
@@ -35,6 +35,7 @@ Scanner sc=new Scanner(System.in);
 import java.util.Scanner;
 
 class TaskA1 {
+
     public static void main (String [] args){
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
