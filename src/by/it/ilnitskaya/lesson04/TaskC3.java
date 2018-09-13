d8798275df0801fe578d36da3db94e4c510a9366
ailnitskaya <asya.ilnitskaya@gmail.com>
2018-09-14 02:38:54 +0300

    lesson5 complete

--- src/by/it/ilnitskaya/lesson04/TaskC3.java
+++ src/by/it/ilnitskaya/lesson04/TaskC3.java
@@ -24,5 +24,20 @@ package by.it.ilnitskaya.lesson04;
 */
 
 public class TaskC3 {
-
+    public static void main(String[] args) {
+        { int i=1,n=1;
+            while(i<=10)
+            {
+                System.out.print(i+" ");
+                while(n<=9)
+                {
+                    n++;
+                    System.out.print(i*n+" ");
+                }
+                i++;
+                n=1;
+                System.out.println("");
+            }
+        }
+    }
 }
