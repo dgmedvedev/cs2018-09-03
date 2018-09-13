d0b124df45a5b054a89cbef0645f26465b1443c5
KsyushaDrebenchuk <drebenchuk.ksysha@gmail.com>
2018-09-13 22:49:24 +0300

    lesson 05 task A complete

--- src/by/it/drebenchuk/lesson05/TaskA1.java
+++ src/by/it/drebenchuk/lesson05/TaskA1.java
@@ -24,8 +24,18 @@ package by.it.drebenchuk.lesson05;
 */
 
 
+import java.util.Scanner;
 
 public class TaskA1 {
-
+    public static void main(String[] args) {
+        int [] mas=new int [10];
+        Scanner scanner= new Scanner(System.in);
+        for (int i = 9; i >= 0; i--) {
+            mas[i]=scanner.nextInt();
+        }
+        for (int element:mas){
+            System.out.println(element);
+        }
+    }
     }
 
