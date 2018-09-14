1e0e8a79cac8df386876af02f0749aff8fbd34ff
ivan <shiloivan1997@gmail.com>
2018-09-14 11:53:17 +0300

    lessonABC

--- src/by/it/shilo/lesson06/TaskB1.java
+++ src/by/it/shilo/lesson06/TaskB1.java
@@ -27,14 +27,14 @@ double averageAge(Dog[] dogs){.....}; //должен вычислять сред
 
 public class TaskB1 {
     public static void main(String[] args) {
-        Dog[] dogs={
+        Dog[] dogs = {
                 new Dog("Шарик", 1),
                 new Dog("Жучка", 2),
                 new Dog("Бобик", 3),
                 new Dog("Барбос", 4),
                 new Dog("Полкан", 5),
         };
-DogHelper.printAllNames(dogs);
+        DogHelper.printAllNames(dogs);
         System.out.println(DogHelper.averageAge(dogs));
     }
 }
