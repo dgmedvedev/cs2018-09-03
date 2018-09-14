ea9e47a613e2d292f51ef011ee813fdbb787e666
ailnitskaya <asya.ilnitskaya@gmail.com>
2018-09-14 12:08:47 +0300

    lesson5 complete

---
+++ src/by/it/ilnitskaya/lesson06/Dog.java
@@ -0,0 +1,24 @@
+package by.it.ilnitskaya.lesson06;
+
+public class Dog {
+    public String getName;
+    public String getAge;
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
