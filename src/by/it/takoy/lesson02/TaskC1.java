c78a93b9fd26baeabe37644c3bc1034b402908af
Takoy <takoyegor@gmail.com>
2018-09-16 01:12:11 +0300

    Lesson 1 - Done Lesson 2 - A1 A2 A3 B1 B2 B3 C1 C3

--- src/by/it/takoy/lesson02/TaskC1.java
+++ src/by/it/takoy/lesson02/TaskC1.java
@@ -17,7 +17,7 @@ class TaskC1 {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int sum = a+b;
-        System.out.println(sum+" ");
+        System.out.println("Sum = "+sum);
     }
 
 }
