b2ca60a2febda09820ab2e5ac96deaa3cf5b6501
Администратор <dg-medvedev@mail.ru>
2019-01-18 13:01:56 +0300

    Home Work 1

--- src/Lesson1/Task1_2.java
+++ src/Lesson1/Task1_2.java
@@ -1,9 +1,11 @@
 package Lesson1;
 
+import java.util.Random;
+
 public class Task1_2 {
     public static void main(String[] args) {
 
-        int number = (int) (Math.random()*1000);
+        int number = (new Random().nextInt());
         System.out.println("Случайное число = "+number);
 
         System.out.println(number % 10 == 7 ? "Последняя цифра равна 7" : "Последняя цифра не равна 7");
