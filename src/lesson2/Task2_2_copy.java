7c7b5320c884a2743b002daa0f0ad8fac2652a70
Администратор <dg-medvedev@mail.ru>
2019-01-19 23:00:25 +0300

    Home Work 2 (complete 1,2)

--- src/lesson2/Task2_2_copy.java
+++ src/lesson2/Task2_2_copy.java
@@ -1,13 +1,16 @@
 package lesson2;
 
-public class Task2_2 {
+import java.util.ArrayList;
+
+public class Task2_2_copy {
 
     public static void main(String[] args) {
         float[] mas = {2, 3, 5, 7, 6, 5, 7, 3, 7, 20};
-        int[] intMasOne = new int[mas.length];
-        int[] intMasTwo = new int[mas.length];
+        int[]intMasOne = new int[mas.length];
+        int[]intMasTwo = new int[mas.length];
+        ArrayList<Integer> intList=new ArrayList<>();
 
-        for (int i = 0; i < mas.length; i++) {
+        for(int i=0;i<mas.length;i++) {
             intMasOne[i] = (int) mas[i];
             intMasTwo[i] = (int) mas[i];
         }
@@ -19,10 +22,14 @@ public class Task2_2 {
             for (int j = 0; j < mas.length; j++) {
                 if (intMasOne[i] == intMasTwo[j]) {
                     count++;
-                    intMasTwo[j] = 0;
+                    intMasTwo[j]=0;
                 }
             }
-            if (count > 1) System.out.println("[" + intMasOne[i] + "]" + " - повторений " + count);
+            intList.add(i,intMasOne[i]);
+
+            if(count>1)
+
+                System.out.println(intList.get(i) + " - повторений " + count);
             count = 0;
         }
     }
