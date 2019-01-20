f28bc62f8549bc7c227f52246921499c63664d13
Администратор <dg-medvedev@mail.ru>
2019-01-20 18:07:34 +0300

    Home Work 2 (complete 1,2,3,4)

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,8 @@
-import lesson2.Task2_1;
-import lesson2.Task2_2;
-import lesson2.Task2_2_copy;
-import lesson2.Task2_3;
+import lesson2.*;
 
 public class Main {
 
     public static void main(String[] args) {
-Task2_3.start();
+Task2_4.start();
     }
 }
