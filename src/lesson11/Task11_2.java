761ab87d83da8eb66bffd738b6d00a88f7c77409
Администратор <dg-medvedev@mail.ru>
2019-03-02 02:33:40 +0300

    Home Work 11

--- src/lesson11/Task11_2.java
+++ src/lesson11/Task11_2.java
@@ -22,28 +22,50 @@ import org.w3c.dom.Element;
 
 public class Task11_2 {
 
-    private static final String FILENAME = "manArrayList.xml";
+    private static final String FILENAME1 = "manArrayList.xml";
+    private static final String FILENAME2 = "myClassArrayList.xml";
 
     public static void start() {
         Task11_1.start();
         System.out.println();
         System.out.println("ЗАДАНИЕ 11_2");
         try {
-            Document document = DocumentBuilderFactory.newInstance()
+            Document document1 = DocumentBuilderFactory.newInstance()
+                    .newDocumentBuilder().newDocument();
+            Document document2 = DocumentBuilderFactory.newInstance()
                     .newDocumentBuilder().newDocument();
 
-            // Корневой элемент
-            Element database = document.createElement("database");
-            document.appendChild(database);
+            // Корневой элемент1
+            Element database = document1.createElement("database");
+            document1.appendChild(database);
+            // Корневой элемент2
+            Element application = document2.createElement("application");
+            document2.appendChild(application);
 
             // Элемент типа students
-            Element students = document.createElement("students");
+            Element students = document1.createElement("students");
             database.appendChild(students);
+
+            //Создание myClassArrayList.xml
+            for (int i = 0; i < Task11_1.getMyClassArrayList().size(); i++) {
+                // Элемент типа class
+                Element clas = document2.createElement("class");
+                application.appendChild(clas);
+
+                clas.setAttribute("name", Task11_1.getMyClassArrayList().get(i).getName());
+                for (int j = 0; j < Task11_1.getMyClassArrayList().get(i).getMethodList().size(); j++) {
+                    Element method = document2.createElement("method");
+
+                    clas.appendChild(method);
+                    method.setAttribute("name", Task11_1.getMyClassArrayList().get(i).getMethodList().get(j).getName());
+                    method.setTextContent(Task11_1.getMyClassArrayList().get(i).getMethodList().get(j).getValue());
+                }
+            }
 
             for (Man man : Task11_1.getManArrayList()) {
                 if (man instanceof Student) {
                     // Элемент типа student
-                    Element student = document.createElement("student");
+                    Element student = document1.createElement("student");
                     students.appendChild(student);
                     // Определяем имя
                     student.setAttribute("name", man.getName());
@@ -57,13 +79,13 @@ public class Task11_2 {
                 }
             }
             // Элемент типа professors
-            Element professors = document.createElement("professors");
+            Element professors = document1.createElement("professors");
             database.appendChild(professors);
 
             for (Man man : Task11_1.getManArrayList()) {
                 if (man instanceof Professor) {
                     // Элемент типа professor
-                    Element professor = document.createElement("professor");
+                    Element professor = document1.createElement("professor");
                     professors.appendChild(professor);
 
                     professor.setAttribute("name", man.getName());
@@ -73,13 +95,13 @@ public class Task11_2 {
             }
 
             // Элемент типа service
-            Element service = document.createElement("service");
+            Element service = document1.createElement("service");
             database.appendChild(service);
 
             for (Man man : Task11_1.getManArrayList()) {
                 if (man instanceof Member) {
                     // Элемент типа member
-                    Element member = document.createElement("member");
+                    Element member = document1.createElement("member");
                     service.appendChild(member);
 
                     member.setAttribute("name", man.getName());
@@ -90,18 +112,23 @@ public class Task11_2 {
             // Сохранить текстовое представление XML документа в файл
             Transformer transformer = TransformerFactory.newInstance()
                     .newTransformer();
-            DOMSource source = new DOMSource(document);
-            StreamResult result = new StreamResult(
+            DOMSource source1 = new DOMSource(document1);
+            DOMSource source2 = new DOMSource(document2);
+            StreamResult result1 = new StreamResult(
                     new File(System.getProperty("user.dir")
-                            + File.separator + FILENAME));
+                            + File.separator + FILENAME1));
+            StreamResult result2 = new StreamResult(
+                    new File(System.getProperty("user.dir")
+                            + File.separator + FILENAME2));
 
             // Для соображений отладки можно вывести результат работы
             // программы на стандартный вывод
             // StreamResult result = new StreamResult(System.out);
 
-            transformer.transform(source, result);
+            transformer.transform(source1, result1);
+            transformer.transform(source2, result2);
 
-            System.out.println("Документ сохранен!");
+            System.out.println("Документы сохранены!");
 
         } catch (ParserConfigurationException
                 | TransformerConfigurationException ex) {
