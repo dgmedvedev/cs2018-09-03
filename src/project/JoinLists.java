391b4edf61f0800f7adead3b5b522a983a793a97
Администратор <dg-medvedev@mail.ru>
2019-04-02 13:13:51 +0300

    Final project

--- src/project/JoinLists.java
+++ src/project/JoinLists.java
@@ -2,49 +2,54 @@ package project;
 
 import project.category.Category;
 
-import java.util.InputMismatchException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public abstract class JoinLists {
-    private static LinkedList<Category> categoryList = new LinkedList<>();
+    private static List<Category> categoryList = new LinkedList<>();
 
     public static void choiceCategory(List<Category> list_1, List<Category> list_2) {
-        if (list_1.get(0).getIdCategory() == list_2.get(0).getIdCategory()) {
-            System.out.println("Найдено совпадение по idCategory");
-            System.out.println("Введите номер категории, которую хотите оставить:");
-            System.out.println("1. " + "\"" + list_1.get(0).getTitle() + "\"" + " из списка 1;");
-            System.out.println("2. " + "\"" + list_2.get(0).getTitle() + "\"" + " из списка 2.");
-            int number = 0;
-            boolean temp = true;
-            while (temp) {
-                try {
-                    Scanner sc = new Scanner(System.in);
-                    number = sc.nextInt();
-                    if (number == 1 || number == 2) {
-                        temp = false;
-                        break;
+
+        for (Category aList_1 : list_1) {
+            for (Category aList_2 : list_2) {
+
+                if (aList_1.getIdCategory() == aList_2.getIdCategory()) {
+                    System.out.println("Найдено совпадение по idCategory");
+                    System.out.println("Введите номер категории, которую хотите оставить:");
+                    System.out.println("1. " + "\"" + aList_1.getTitle() + "\"" + " из списка 1. IdCategory = " + aList_1.getIdCategory() + ";");
+                    System.out.println("2. " + "\"" + aList_2.getTitle() + "\"" + " из списка 2. IdCategory = " + aList_2.getIdCategory() + ".");
+                    int number = 0;
+                    boolean temp = true;
+                    while (temp) {
+                        try {
+                            Scanner sc = new Scanner(System.in);
+                            number = sc.nextInt();
+                            if (number == 1 || number == 2) {
+                                temp = false;
+                                break;
+                            }
+                        } catch (InputMismatchException ime) {
+                            System.out.println("Будьте внимательнее!");
+                        }
+                        System.out.println("Введено неверное значение! Введите 1 или 2");
                     }
-                } catch (InputMismatchException ime) {
-                    System.out.println("Будьте внимательнее!");
+
+                    if (number == 1) {
+                        categoryList.add(aList_1);
+                        System.out.println("\n" + "Выполнено успешно.");
+                    }
+
+                    if (number == 2) {
+                        categoryList.add(aList_2);
+                        System.out.println("\n" + "Выполнено успешно.");
+                    }
                 }
-                System.out.println("Введено неверное значение! Введите 1 или 2");
-            }
-
-            if (number == 1) {
-                categoryList.add(list_1.get(0));
-                System.out.println("\n" + "Выполнено успешно.");
-            }
-
-            if (number == 2) {
-                categoryList.add(list_2.get(0));
-                System.out.println("\n" + "Выполнено успешно.");
             }
         }
+
+        
     }
 
-    public static LinkedList<Category> getCategoryList() {
+    public static List<Category> getCategoryList() {
         return categoryList;
     }
 }
--- src/project/temp/TestDrive.java
+++ src/project/temp/TestDrive.java
@@ -2,10 +2,6 @@ package project.temp;
 
 import project.JoinLists;
 import project.category.Category;
-import project.json.JsonReader;
-import project.json.UrlString;
-
-import java.util.List;
 
 public class TestDrive {
     public static void start() {
@@ -15,16 +11,17 @@ public class TestDrive {
         JSON_1.start();
         JSON_2.start();
 
-        for(Category category : JSON_1.getCategoryList_1())
+    /*    for(Category category : JSON_1.getCategoryList_1())
             System.out.println(category);
 
         for(Category category : JSON_2.getCategoryList_2())
             System.out.println(category);
-
+*/
         JoinLists.choiceCategory(JSON_1.getCategoryList_1(),JSON_2.getCategoryList_2());
 
         for(Category category : JoinLists.getCategoryList())
             System.out.println(category);
+
 
     }
 }
